3ac782c45dee96c7412f4c0cd202382ff7346eab
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2024-08-03 00:10:07 +0300

    Add Student Exam DTO

---
+++ SmartExam.Application/DTOs/StudentExam/AddStudentExamDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartExam.Application.DTOs.StudentExam
+{
+    public record AddStudentExamDTO (
+        string FullName,
+        string Email,
+        int ExamId
+    );
+}
---
+++ SmartExam.Application/DTOs/StudentExam/StudentExamDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartExam.Application.DTOs.StudentExam
+{
+    public record StudentExamDTO (
+      int Id,
+      string FullName,
+      string Email,
+      int ExamId,
+      object? Exam
+    );
+}
