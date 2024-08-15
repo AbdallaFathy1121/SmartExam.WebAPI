8da42331b27465e85c9195a87cf1490702b96558
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2024-08-15 13:17:38 +0300

    Add StudentAnswerController

---
+++ SmartExam.API/Controllers/StudentAnswersController.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using SmartExam.Application.DTOs.ApiResponse;
+using SmartExam.Application.DTOs.StudentAnswer;
+using SmartExam.Application.DTOs.StudentExamQuestion;
+using SmartExam.Application.Interfaces.Repositories;
+using SmartExam.Domain.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartExam.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAnswersController : ControllerBase
+    {
+        private IValidator<StudentAnswer> _validator;
+        private IUnitOfWork _unitOfWork;
+        private IMapper _mapper;
+        public StudentAnswersController(IUnitOfWork unitOfWork, IMapper mapper, IValidator<StudentAnswer> validator)
+        {
+            _unitOfWork = unitOfWork;
+            _validator = validator;
+            _mapper = mapper;
+        }
+
+
+        // GET: api/<StudentAnswersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<StudentAnswersController>/ Add
+        [HttpPost("AddList")]
+        public async Task<IActionResult> Add([FromBody] IList<AddStudentAnswerDTO> dtos)
+        {
+            ApiResponse<IList<StudentAnswerDTO>> response = new ApiResponse<IList<StudentAnswerDTO>>();
+
+            StudentExam findStudnetExamById = await _unitOfWork.StudentExamRepository.GetByIdAsync(dtos[0].StudentExamId);
+            if (findStudnetExamById is null)
+            {
+                response.ErrorMessages!.Add("Invalid Student Exam Id");
+                return BadRequest(response);
+            }
+
+            StudentAnswer findStudentAnswerByStudentExamId = await _unitOfWork.StudentAnswerRepository.GetFirstAsync(a => a.StudentExamId == dtos[0].StudentExamId);
+            if (findStudentAnswerByStudentExamId is not null)
+            {
+                response.ErrorMessages!.Add("Student Exam Id is Already existe");
+                return BadRequest(response);
+            }
+
+            IList<StudentAnswer> studentAnswers = new List<StudentAnswer>();
+
+            foreach (var item in dtos)
+            {
+                Question getQuestionById = await _unitOfWork.QuestionRepository.GetByIdAsync(item.QuestionId);
+                if (getQuestionById is null)
+                {
+                    response.ErrorMessages!.Add("Invalid Question Id");
+                    return BadRequest(response);
+                }
+
+                bool isCorrect = false;
+                if (getQuestionById.CorrectAnswer == item.SelectedAnswer)
+                {
+                    isCorrect = true;
+                }
+
+                StudentAnswer studentAnswer = new StudentAnswer { 
+                    StudentExamId =  item.StudentExamId, 
+                    QuestionId = item.QuestionId, 
+                    SelectedAnswer = item.SelectedAnswer,
+                    IsCorrect = isCorrect
+                };
+
+                studentAnswers.Add(studentAnswer);
+            }
+
+            // Save In Database
+            await _unitOfWork.StudentAnswerRepository.AddRangeAsync(studentAnswers);
+            await _unitOfWork.CompleteAsync();
+
+            // AutoMapper
+            IList<StudentAnswerDTO> data = _mapper.Map<IList<StudentAnswerDTO>>(studentAnswers);
+
+            response.IsSuccess = true;
+            response.Data = data;
+            return Ok(response);
+        }
+    }
+}
---
+++ SmartExam.Infrastructure/Migrations/20240815101159_Add StudentAnswer Entity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartExam.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentAnswerEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentAnswers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentExamId = table.Column<int>(type: "int", nullable: false),
+                    QuestionId = table.Column<int>(type: "int", nullable: false),
+                    SelectedAnswer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsCorrect = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentAnswers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudentAnswers_Questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentAnswers_StudentExams_StudentExamId",
+                        column: x => x.StudentExamId,
+                        principalTable: "StudentExams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentAnswers_QuestionId",
+                table: "StudentAnswers",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentAnswers_StudentExamId",
+                table: "StudentAnswers",
+                column: "StudentExamId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentAnswers");
+        }
+    }
+}
